9c2d2e2220b108ec7f47329d3fe251ac05c0bcae
pokeduck <40366083+pokeduck@users.noreply.github.com>
2024-04-12 18:40:50 +0800

    part 5 student

--- Models/Students.cs
+++ Models/Students.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -6,9 +7,27 @@ namespace ContosoUniversity.Models
     {
         [Key]
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name Name")]
         public string? LastName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First name can't be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name N")]
         public string? FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
         public ICollection<Enrollment>? Enrollments { get; set; }
     }
 
